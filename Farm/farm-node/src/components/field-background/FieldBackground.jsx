a16f1a36599a458b97d06030573fa62f7928cdaa
Mgilarmas <mauriciogilarmas@gmail.com>
2024-04-09 15:13:27 +0100

    Searh bar, logo, all in place

--- Farm/farm-node/src/components/field-background/FieldBackground.jsx
+++ Farm/farm-node/src/components/field-background/FieldBackground.jsx
@@ -1,17 +1,26 @@
 import "./FieldBackground.css"
 
 function FieldBackground() {
-    return (
-        <>
-            <div className="background-image">
-            <div className="yellow-filter">
-                <h1>Star</h1>
-                <h1>Crops</h1>
-            </div>
-            </div>
-            
-        </>
-    )
+  return (
+    <>
+      <div className="logo">
+        <div className="logo-and-title">
+          <img src="/images/starcrop-logo.png" alt="logo" />
+          <div>
+            <h1>Star</h1>
+            <h1>Crops</h1>
+          </div>
+        </div>
+        <input type="text" />
+      </div>
+      <div className="background-image">
+        <div>
+          <div className="yellow-filter"></div>
+        </div>
+      </div>
+
+    </>
+  )
 }
 
 export default FieldBackground;
--- Farm/farm-node/src/components/header/Header.jsx
+++ Farm/farm-node/src/components/header/Header.jsx
@@ -1,11 +1,11 @@
 import "./Header.css"
 
-function Header(){
-    return(
-        <div className="header">
-            <i class="fa-solid fa-bars"></i>
-        </div>
-    )
+function Header() {
+  return (
+    <div className="header">
+      <i class="fa-solid fa-bars"></i>
+    </div>
+  )
 }
 
 export default Header;
