54223b0bfdab294b1e3f1e2f4472b243a7110ec0
Mauricio <mauriciogilarmas@gmail.com>
2024-04-18 16:13:40 +0100

    Finished the contact page

--- Farm/farm-node/src/components/contact-map/ContactMap.jsx
+++ Farm/farm-node/src/components/contact-map/ContactMap.jsx
@@ -3,11 +3,23 @@ import "./ContactMap.css";
 function ContactMap(){
   return (
     <>
-      <div className="map-container">
-        <div id="map">
-          
+    <div className="background">
+    <div className="map-container">
+        <h1>Conocenos</h1>
+        <img src="/images/map.png" alt="map" />
+      </div>
+      <div className="formulary-container">
+        <div className="formulary-personal-data">
+          <input type="text" placeholder="Your name"/>
+          <input type="number" placeholder="Your phone number"/>
+          <input type="email" placeholder="Your Email"/>
+        </div>
+        <div className="formulary-message">
+          <input type="text" placeholder="Your message"/>
+          <button>Submit</button>
         </div>
       </div>
+    </div>
     </>
   )
 }
--- Farm/farm-node/src/pages/contact/Contact.jsx
+++ Farm/farm-node/src/pages/contact/Contact.jsx
@@ -1,11 +1,13 @@
 import ContactMap from "../../components/contact-map/ContactMap";
 import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
 
 function Contact() {
   return (
     <>
       <Header />
       <ContactMap />
+      <Footer />
     </>
   )
 }
