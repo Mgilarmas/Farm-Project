909f9336a28e421df6feca9177e965ac1013e1ec
Mauricio <mauriciogilarmas@gmail.com>
2024-05-15 20:30:07 +0100

    Added images to the spheres

--- src/components/seeds-types/SeedsTypes.jsx
+++ src/components/seeds-types/SeedsTypes.jsx
@@ -10,22 +10,16 @@ function SeedsTypes() {
             <img src="/images/roses.jpeg" alt="Rosas" />
             <div className="spheres">
               <div className="spheres-seeds">
-
+                <img src="/images/spring_flowers.jfif" alt="Sprin Flowers" />
               </div>
               <div className="spheres-seeds">
-
+                <img src="/images/summer_flowers.jfif" alt="Summer Flowers" />
               </div>
               <div className="spheres-seeds">
-
+                <img src="/images/autum_flowers.jfif" alt="Autum Flowers" />
               </div>
               <div className="spheres-seeds">
-
-              </div>
-              <div className="spheres-seeds">
-
-              </div>
-              <div className="spheres-seeds">
-                
+                <img src="/images/winter_flowers.jfif" alt="Winter Flowers" />
               </div>
             </div>
           </div>
@@ -35,16 +29,19 @@ function SeedsTypes() {
             <img src="/images/manzano.jpg" alt="Manzano" />
             <div className="spheres">
               <div className="spheres-seeds">
-
+                <img src="/images/fruit_tree.jfif" alt="Fruit Tree" />
               </div>
               <div className="spheres-seeds">
-
+                <img src="/images/fruit_bush.jfif" alt="Fruit Bush" />
               </div>
               <div className="spheres-seeds">
-
+                <img src="/images/bananas.jfif" alt="Bananas" />
               </div>
               <div className="spheres-seeds">
-
+                <img src="/images/vineyard.jfif" alt="Vineyard" />
+              </div>
+              <div className="spheres-seeds">
+                <img src="/images/watermelon.jfif" alt="Watermelon Plant" />
               </div>
             </div>
           </div>
@@ -54,10 +51,22 @@ function SeedsTypes() {
             <img src="/images/berenjenas.jpg" alt="Berenjenas" />
             <div className="spheres">
               <div className="spheres-seeds">
-
+                <img src="/images/broccoli.jfif" alt="Broccoli" />
               </div>
               <div className="spheres-seeds">
-
+                <img src="/images/garlic.jfif" alt="Garlic" />
+              </div>
+              <div className="spheres-seeds">
+                <img src="/images/lettuce.jfif" alt="Lettuce" />
+              </div>
+              <div className="spheres-seeds">
+                <img src="/images/peas.jfif" alt="Peas" />
+              </div>
+              <div className="spheres-seeds">
+                <img src="/images/pepper.jfif" alt="Pepper" />
+              </div>
+              <div className="spheres-seeds">
+                <img src="/images/pumpkin.jfif" alt="Pumpkin" />
               </div>
             </div>
           </div>
@@ -67,13 +76,13 @@ function SeedsTypes() {
             <img src="/images/olive-tree.jpg" alt="Olivo" />
             <div className="spheres">
               <div className="spheres-seeds">
-
+              <img src="/images/ash_tree.jfif" alt="Ash tree" />
               </div>
               <div className="spheres-seeds">
-
+              <img src="/images/fir_tree.jfif" alt="Fir Tree" />
               </div>
               <div className="spheres-seeds">
-
+              <img src="/images/palm_tree.jfif" alt="Palm Tree" />
               </div>
             </div>
           </div>
