500cfd977accca82b2aa9de6c49bd456a2834da6
Mauricio <mauriciogilarmas@gmail.com>
2024-05-15 16:14:16 +0100

    Added backgroun to SeedsTypes, finished hover animation, RSS and added spheres

--- src/components/seeds-types/SeedsTypes.jsx
+++ src/components/seeds-types/SeedsTypes.jsx
@@ -3,30 +3,83 @@ import "./SeedsTypes.css"
 function SeedsTypes() {
   return (
     <>
-      <div className="seeds-types-top-triangle"></div>
       <div className="seeds-types-container">
+        <div className="seeds-types-top-triangle"></div>
         <div className="seeds-types-container-variety">
           <div className="seeds-types-container-seeds">
-            <img src="/images/roses.jpeg" alt="Roses" />
+            <img src="/images/roses.jpeg" alt="Rosas" />
+            <div className="spheres">
+              <div className="spheres-seeds">
+
+              </div>
+              <div className="spheres-seeds">
+
+              </div>
+              <div className="spheres-seeds">
+
+              </div>
+              <div className="spheres-seeds">
+
+              </div>
+              <div className="spheres-seeds">
+
+              </div>
+              <div className="spheres-seeds">
+                
+              </div>
+            </div>
           </div>
         </div>
         <div className="seeds-types-container-variety">
           <div className="seeds-types-container-seeds">
-          <img src="/images/manzano.jpg" alt="Roses" />
+            <img src="/images/manzano.jpg" alt="Manzano" />
+            <div className="spheres">
+              <div className="spheres-seeds">
+
+              </div>
+              <div className="spheres-seeds">
+
+              </div>
+              <div className="spheres-seeds">
+
+              </div>
+              <div className="spheres-seeds">
+
+              </div>
+            </div>
           </div>
         </div>
         <div className="seeds-types-container-variety">
           <div className="seeds-types-container-seeds">
-          <img src="/images/berenjenas.jpg" alt="Roses" />
+            <img src="/images/berenjenas.jpg" alt="Berenjenas" />
+            <div className="spheres">
+              <div className="spheres-seeds">
+
+              </div>
+              <div className="spheres-seeds">
+
+              </div>
+            </div>
           </div>
         </div>
         <div className="seeds-types-container-variety">
           <div className="seeds-types-container-seeds">
-          <img src="/images/olive-tree.jpg" alt="Roses" />
+            <img src="/images/olive-tree.jpg" alt="Olivo" />
+            <div className="spheres">
+              <div className="spheres-seeds">
+
+              </div>
+              <div className="spheres-seeds">
+
+              </div>
+              <div className="spheres-seeds">
+
+              </div>
+            </div>
           </div>
         </div>
+        <div className="seeds-types-bottom-triangle"></div>
       </div>
-      <div className="seeds-types-bottom-triangle"></div>
     </>
   )
 }
